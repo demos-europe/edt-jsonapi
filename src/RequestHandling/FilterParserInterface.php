7fa534271e6a94a2b45216c4af335553fa1e41ea
GitHub <action@github.com>
2024-02-13 14:05:30 +0000

    prepare release

--- src/RequestHandling/FilterParserInterface.php
+++ src/RequestHandling/FilterParserInterface.php
@@ -20,4 +20,11 @@ interface FilterParserInterface
      * @throws FilterException
      */
     public function parseFilter($filter): array;
+
+    /**
+     * @return TFilter
+     *
+     * @deprecated call a validator manually, that asserts that the type of $filter matches the type required by {@link self::parseFilter()}
+     */
+    public function validateFilter(mixed $filter);
 }
